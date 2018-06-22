1d24d95c188a32fa0a3be5a4cf4eebb08c5864f9
misterds <misterds1@mail.ru>
2018-06-22 16:14:54 -0400

    Creating  the Skils Activity

--- app/src/main/java/com/example/serj/smoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/serj/smoosh/LeagueActivity.kt
@@ -1,7 +1,9 @@
 package com.example.serj.smoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 
 //we inheriten the class BaseActivity()
@@ -11,5 +13,12 @@ class LeagueActivity : BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view:View){
+
+//         create an Intent SkillsActivity
+        val skilActivity = Intent(this, SkillsActivity::class.java)
+        startActivity(skilActivity)
     }
 }
