3580704d3734c6c5fca72d00ff18f12de47238ed
misterds <misterds1@mail.ru>
2018-06-22 14:43:22 -0400

    Create the explisite Intent

--- app/src/main/java/com/example/serj/smoosh/WelcomActivity.kt
+++ app/src/main/java/com/example/serj/smoosh/WelcomActivity.kt
@@ -1,12 +1,23 @@
 package com.example.serj.smoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcom.*
 
 class WelcomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcom)
+
+//        add click event
+
+        getStartedButton.setOnClickListener {
+//            creating   explisite Intent
+            val leagueIntent = Intent(this,LeagueActivity::class.java)
+//            start  explisite Intent   with startActivity method
+            startActivity(leagueIntent)
+        }
     }
 }
